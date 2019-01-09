9351826259d5929e07f8165fe4611b32cb038ba8
Allain Bernal <llnbrnl017@gmail.com>
2019-01-08 16:25:24 -0800

    Added sequelize code for FamilyPlanningDB, users table and calender table. Also included testdb just incase(?)

---
+++ models/calendar.js
@@ -0,0 +1,49 @@
+module.exports = function(sequelize, DataTypes) {
+    var Calendar = sequelize.define("Calendar", {
+        activity_name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            len: [1,30],
+        },
+
+        fees: {
+            type: DataTypes.INTEGER,
+            len: [1],
+            isInt: true,
+        },
+
+        location_street: {
+            type: DataTypes.STRING,
+            len: [1,30],
+            isAlphanumeric: true, //sets input to only accept numbers and letters
+        },
+
+        location_city: {
+            type: DataTypes.STRING,
+            len: [1,30],
+            isAlpha: true, //sets input to only accept letters
+        },
+
+        location_state: {
+            type: DataTypes.STRING,
+            len: [2,2],
+            isAlpha: true, //sets input to only accept letters
+        },
+
+        location_zip: {
+            type: DataTypes.INTEGER,
+            len: [5],
+            isNumeric: true, //sets input to only accept numbers
+        },
+    }); //end of var Calendar
+
+    Post.associate = function(models) {
+        Post.belongsTo(models.Users, {
+            foreignKey: {
+                allowNull: false
+            }, //end foreignKey
+        }) //end Post.belongsTo
+    }; //end Post.associate
+
+    return Calendar;
+}; //end module.exports
