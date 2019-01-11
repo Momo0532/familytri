42149f9c002a4a207cec14c5b1375533380f46a4
BUMBALORD <davidhallinan@gmail.com>
2019-01-11 10:42:45 -0800

    add

--- public/js/calendarView.js
+++ public/js/calendarView.js
@@ -1,5 +1,22 @@
 $(document).ready(function() {
-  $.get("/api/calendarview").then(function(data) {
-    $(".dateinfo").text(data.activity_name);
+  // page is now ready, initialize the calendar...
+  
+  $('#calendar').fullCalendar({
+    events: [
+      {
+        title  : 'event1',
+        start  : '2019-01-01'
+      },
+      {
+        title  : 'event2',
+        start  : '2019-01-05',
+        end    : '2019-01-07'
+      },
+      {
+        title  : 'event3',
+        start  : '2010-01-09T12:30:00',
+        allDay : false // will make the time show
+      }
+    ]
   });
-});
+      });
